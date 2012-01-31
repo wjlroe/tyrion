e433fff3e4af7a47c894a2ff0a43fa240c9ef5b8
William Roe <willroe@gmail.com>
2012-01-31 15:18:54 +0000

    Fix config parsing mistake

--- tyrion/config.py
+++ tyrion/config.py
@@ -11,11 +11,11 @@ class Config:
         self.rootdir = None
 
     def read_config(self, path):
-        config = ConfigParser.RawConfigParser()
-        config.read(path)
-        self.hostname = config.get('amqp', 'hostname', '127.0.0.1')
-        self.username = config.get('amqp', 'username', '')
-        self.password = config.get('amqp', 'password', '')
-        self.exchange = config.get('amqp', 'exchange', '')
-        self.vhost = config.get('amqp', 'vhost', '/')
-        self.rootdir = config.get('tyrion', 'rootdir', '/tmp/deploy')
+        configs = ConfigParser.RawConfigParser()
+        configs.read(path)
+        self.hostname = configs.get('amqp', 'hostname')
+        self.username = configs.get('amqp', 'username')
+        self.password = configs.get('amqp', 'password')
+        self.exchange = configs.get('amqp', 'exchange')
+        self.vhost = configs.get('amqp', 'vhost')
+        self.rootdir = configs.get('tyrion', 'rootdir')
